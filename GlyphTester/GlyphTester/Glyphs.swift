7304c7c97a0845a93571650641e1bc0cc7f45e2d
Aaron Madlon-Kay <aaron@madlon-kay.com>
2016-10-25 17:49:22 +0900

    Fix selector for iOS 8

--- GlyphTester/GlyphTester/Glyphs.swift
+++ GlyphTester/GlyphTester/Glyphs.swift
@@ -75,7 +75,7 @@ class Glyphs {
         let text = NSTextStorage(attributedString: NSAttributedString(string: string))
         let layoutManager = NSLayoutManager()
         text.addLayoutManager(layoutManager)
-        let glyph = layoutManager.cgGlyph(at: 0)
+        let glyph = layoutManager.glyph(at: 0)
         let attr = text.attribute(NSFontAttributeName, at: 0, longestEffectiveRange: nil, in: NSMakeRange(0, 1))
         guard let font = attr as? UIFont else {
             throw CodePointNamingError.nameUnavailable("failed to get font")
