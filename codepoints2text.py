0dd02ac1e22ae3826341e3c8d8bc1febd746cc70
Aaron Madlon-Kay <aaron@madlon-kay.com>
2022-12-15 10:14:02 +0900

    Correctly report removed codepoints separately from added ones

--- codepoints2text.py
+++ codepoints2text.py
@@ -7,13 +7,16 @@ format) to get a string of actual text
 import sys
 
 for line in sys.stdin:
-    _, hx = line.strip().split('+')
-    n = int(hx, base=16)
-    try:
-        char = unichr(n)
-        out = char.encode('utf-8')
-    except NameError:
-        out = chr(n)
+    if line.startswith(('U+', 'u+')):
+        _, hx = line.strip().split('+')
+        n = int(hx, base=16)
+        try:
+            char = unichr(n)
+            out = char.encode('utf-8')
+        except NameError:
+            out = chr(n)
+    else:
+        out = line
     try:
         sys.stdout.write(out)
     except UnicodeEncodeError as e:
